ccbaa1d92cf42d05a196dd76de15d391d3e4e629
fahretf <ffijuljani1@etf.unsa.ba>
2025-06-13 13:07:49 +0200

    Added kubernetes config

--- petclinicfe/docker-entrypoint.sh
+++ petclinicfe/docker-entrypoint.sh
@@ -4,10 +4,12 @@ echo "window.env = {" > /usr/share/nginx/html/config.js
 echo "  REST_API_URL: '${REST_API_URL:-/petclinic/api/}'" >> /usr/share/nginx/html/config.js
 echo "};" >> /usr/share/nginx/html/config.js
 
-BACKEND_URL=${BACKEND_URL:-http://petclinicbe:9966}
+BACKEND_URL=${BACKEND_URL:-http://backend-service:9966}
 sed -i "s|__BACKEND_URL__|${BACKEND_URL}|g" /etc/nginx/conf.d/default.conf
 
-until nc -z petclinicbe 9966; do
+BACKEND_HOST=${BACKEND_HOST:-backend-service}
+BACKEND_PORT=${BACKEND_PORT:-9966}
+until nc -z $BACKEND_HOST $BACKEND_PORT; do
   echo "Wait for backend to be ready"
   sleep 1
 done
